3bace49c225367718a5e98ca8e12d49a968cd423
ireallyliketyping <pillowyarachnid@gmail.com>
2023-08-06 14:03:14 -0400

    adding the generator and updating readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # password_generator
-simple password generator
+simple password generator, including ascii characters, digits, and punctuation
---
+++ generator.py
@@ -0,0 +1,27 @@
+import secrets
+import string
+
+def validInput(length):
+    if (not length.isdigit()):
+        print(f"you need to enter a positive int\n")
+        return 0
+    elif int(float(length)) not in range(1,30,1):
+        print(f"Length needs to be between 1 and 30")
+        return 0
+    else:
+        print(f"Password length will be {length}\n")
+        return int(float(length))
+
+def generate_password(characters, password_length):
+    password = ''.join(secrets.choice(characters) for i in range (1,password_length, 1))
+    return password
+
+def main():
+    desired_length = input("How many characters would you like in your password?\n")
+    valid_characters = string.ascii_letters + string.digits + string.punctuation
+    password_length = validInput(desired_length)
+    password = generate_password(valid_characters, password_length)
+    print(f"Your password is: {password}")
+
+if __name__ == "__main__":
+    main()
